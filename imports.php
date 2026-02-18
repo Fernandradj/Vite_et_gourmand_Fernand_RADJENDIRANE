6befe55bf87e36962e32ea708be80e4724118714
Fernandradj <ferno6randy@gmail.com>
2026-02-18 17:38:56 +0100

    import new classes

--- imports.php
+++ imports.php
@@ -2,13 +2,14 @@
 
 $currentFolder = realpath(dirname(__FILE__));
 include_once("database.php");
-include_once($currentFolder."/modele/Result.php");
+include_once($currentFolder."/modele/Resultat.php");
 include_once($currentFolder."/modele/Utilisateur.php");
 include_once($currentFolder."/modele/Avis.php");
 include_once($currentFolder."/modele/Commande.php");
 include_once($currentFolder."/modele/Menu.php");
 include_once($currentFolder."/modele/Horaire.php");
 include_once($currentFolder."/modele/Produit.php");
+include_once($currentFolder."/modele/Suivi.php");
 session_start();
 
 
