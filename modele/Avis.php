e39788a0a7f9aa946305334e257829b506fe3730
Fernandradj <ferno6randy@gmail.com>
2026-07-06 10:16:35 +0200

    AvisDAO: created DAO for Avis class

--- modele/Avis.php
+++ modele/Avis.php
@@ -19,8 +19,6 @@ class Avis
 
     private int $avis_id;
 
-    private PDO $pdo;
-
     private int $note;
     private string $commentaire;
     private string $statut;
@@ -31,176 +29,32 @@ class Avis
 
     private Commande $commande;
 
-
-
-
-    public function __construct(bool $loadData, int $avis_id, string $statut = "", string $commentaire = "", int $note = 0, ?Utilisateur $soumis_par, ?Utilisateur $valide_refuse_par = null, ?Commande $commande = null, PDO $pdo)
-    {
-        $this->pdo = $pdo;
-        $this->avis_id = $avis_id;
-        if ($loadData) {
-            $sql = "SELECT Avis_Id, Commentaire, Note, Statut, Soumis_par, Valide_refuse_par, Commande FROM avis WHERE Avis_Id =?";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$avis_id]);
-            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            if ($resultat) {
-
-                if ($resultat["Commentaire"] == null) {
-                    $this->commentaire = "";
-                } else {
-                    $this->commentaire = $resultat["Commentaire"];
-                }
-                $this->note = $resultat["Note"];
-                $this->statut = $resultat["Statut"];
-
-                $utilisateur_id = $resultat["Soumis_par"];
-                $this->soumis_par = new Utilisateur(true, $utilisateur_id, $pdo);
-
-                $utilisateur_id = $resultat["Valide_refuse_par"];
-                if ($utilisateur_id != null) {
-                    $this->valide_refuse_par = new Utilisateur(true, $utilisateur_id, $pdo);
-                }
-
-                $commande_id = $resultat["Commande"];
-                $this->commande = new Commande($commande_id, $pdo);
-            }
-        } else {
-            $this->statut = $statut;
-            $this->commentaire = $commentaire;
-            $this->note = $note;
-            if ($soumis_par == null) {
-                $this->soumis_par = new Utilisateur(true, 0, $pdo);
-            } else {
-                $this->soumis_par = $soumis_par;
-            }
-            if ($valide_refuse_par == null) {
-                $this->valide_refuse_par = new Utilisateur(true, 0, $pdo);
-            } else {
-                $this->valide_refuse_par = $valide_refuse_par;
-            }
-            if ($commande == null) {
-                $this->commande = new Commande(0, $pdo);
-            } else {
-                $this->commande = $commande;
-            }
-        }
-    }
-
-    /* public static function loadAvisFromId(int $id, PDO $pdo): Avis
-    {
-        $sql = "SELECT Avis_Id, Commentaire, Note, Statut, Soumis_par, Valide_refuse_par, Commande FROM avis WHERE Avis_Id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$id]);
-        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($resultat) {
-            return new Avis(false, $resultat['Avis_Id'], $resultat['Statut'], $resultat['Commentaire'], $resultat['Note'], new Utilisateur(true, $resultat['Soumis_par'], $pdo), new Utilisateur(true, $resultat['Valide_refuse_par'], $pdo), new Commande($resultat['Commande'], $pdo), $pdo);
-        }
-        return null;
-    } */
-
-    public static function loadAvisIdOfCommande(int $commandeId, PDO $pdo): int
-    {
-        $sql = "SELECT Avis_Id FROM avis WHERE Commande = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$commandeId]);
-        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($resultat) {
-            return $resultat['Avis_Id'];
-        }
-        return 0;
-    }
-
-    public static function loadBestAvis(PDO $pdo): array
-    {
-        // SQL : SELECT Avis_Id, Note, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = 'Validé' ORDER BY Note DESC LIMIT 3
-        $sql = "SELECT Avis_Id, Note, avis.Statut avisStatut, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo, Valide_refuse_par, Commande FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = ? ORDER BY Note DESC LIMIT 3";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([Avis::AVIS_STATUT_VALIDE]);
-        $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // print_r($resultat);
-        $avis = [];
-        if ($resultat) {
-            foreach ($resultat as $value) {
-                $new_avis = new Avis(false, $value["Avis_Id"], $value["avisStatut"], $value["Commentaire"], $value["Note"], new Utilisateur(true, $value["Soumis_par"], $pdo), new Utilisateur(true, $value["Valide_refuse_par"], $pdo), new Commande($value["Commande"], $pdo), $pdo);
-                array_push($avis, $new_avis);
-            }
-        }
-        return $avis;
-    }
-
-    public static function loadAvisAValider(PDO $pdo): array
-    {
-        // SQL : SELECT Avis_Id, Note, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = 'Validé' ORDER BY Note DESC LIMIT 3
-        $sql = "SELECT Avis_Id, Note, avis.Statut avisStatut, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo, Valide_refuse_par, Commande FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = ? ORDER BY Avis_Id DESC";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([Avis::AVIS_STATUT_EN_COURS]);
-        $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // print_r($resultat);
-        $avis = [];
-        if ($resultat) {
-            foreach ($resultat as $value) {
-                $new_avis = new Avis(false, $value["Avis_Id"], $value["avisStatut"], $value["Commentaire"], $value["Note"], new Utilisateur(true, $value["Soumis_par"], $pdo), new Utilisateur(true, 0, $pdo), new Commande($value["Commande"], $pdo), $pdo);
-                array_push($avis, $new_avis);
-            }
-        }
-        return $avis;
-    }
-
-    public static function soumettreAvis(int $commandeId, int $note, string $commentaire, int $utilisateurId, $pdo): Resultat
-    {
-        try {
-            $sql = "INSERT INTO avis (Commentaire, Note, Statut, Commande, Soumis_par) VALUES (?, ?, ?, ?, ?)";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$commentaire, $note, Avis::AVIS_STATUT_EN_COURS, $commandeId, $utilisateurId]);
-            return new Resultat(true, Avis::RESULT_AVIS_SAVED);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-        return new Resultat(false, Avis::RESULT_FAIL);
-    }
-
-    public static function validerAvis(int $avisId, int $employeId, $pdo): Resultat
-    {
-        try {
-            $sql = "UPDATE avis SET Statut = ?, Valide_refuse_par = ? WHERE Avis_Id = ?";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([Avis::AVIS_STATUT_VALIDE, $employeId, $avisId]);
-            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-            return new Resultat(true, Avis::RESULT_AVIS_VALIDATED);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-        return new Resultat(false, Avis::RESULT_FAIL);
-    }
-
-    public static function rejeterAvis(int $avisId, int $employeId, $pdo): Resultat
-    {
-        try {
-            $sql = "UPDATE avis SET Statut = ?, Valide_refuse_par = ? WHERE Avis_Id = ?";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([Avis::AVIS_STATUT_REJETE, $employeId, $avisId]);
-            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
-            return new Resultat(true, Avis::RESULT_AVIS_REJECTED);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-        return new Resultat(false, Avis::RESULT_FAIL);
-    }
-
     public function getId(): int
     {
         return $this->avis_id;
+    }
+    public function setId(int $newId)
+    {
+        $this->avis_id = $newId;
     }
 
     public function getNote(): int
     {
         return $this->note;
     }
+    public function setNote(int $newNote)
+    {
+        $this->note = $newNote;
+    }
 
     public function getCommentaire(): string
     {
         return $this->commentaire;
+    }
+    public function setCommentaire(string $newCommentaire)
+    {
+        $this->commentaire = $newCommentaire;
+
     }
 
 
@@ -221,30 +75,52 @@ class Avis
     {
         return $this->statut;
     }
+    public function setStatut(string $newStatut)
+    {
+        $this->statut = $newStatut;
+    }
 
     public function getSoumisPar(): Utilisateur
     {
         return $this->soumis_par;
     }
-
+    public function setSoumisPar(Utilisateur $newSoumisPar)
+    {
+        $this->soumis_par = $newSoumisPar;
+    }
     public function getSoumisParId(): int
     {
         return $this->soumis_par->getId();
+    }
+    public function setSoumisParId(int $newSoumisParId)
+    {
+        $this->soumis_par->setId($newSoumisParId);
     }
 
     public function getValideRefusePar(): Utilisateur
     {
         return $this->valide_refuse_par;
     }
+    public function setValideRefusePar(Utilisateur $newValideRefusePar)
+    {
+        $this->valide_refuse_par = $newValideRefusePar;
+    }
 
     public function getValideRefuseParId(): int
     {
         return $this->valide_refuse_par->getId();
     }
-
+    public function setValideRefuseParId(int $newValideRefuseParId)
+    {
+        $this->valide_refuse_par->setId($newValideRefuseParId);
+    }
     public function getCommande(): Commande
     {
         return $this->commande;
+    }
+    public function setCommande($newCommande)
+    {
+        $this->commande = $newCommande;
     }
 
     // public function isAvisPaye(): bool
@@ -256,7 +132,9 @@ class Avis
     {
         return ($this->getStatut() == Avis::AVIS_STATUT_EN_COURS);
     }
-
+    // public function set isAvisEnCours(bool $newisAvisEnCours) {
+//     $this->isAvisEnCours = $newisAvisEnCours;
+// }
     // public function isAvisEnAnnule(): bool
     // {
     //     return ($this->getStatut() == Avis::AVIS_STATUT_ANNULE);
@@ -266,21 +144,33 @@ class Avis
     {
         return ($this->getStatut() == Avis::AVIS_STATUT_VALIDE);
     }
+    // public function setisAvisValide(bool $newisAvisValide){
+    //     $this->isAvisValide = $newisAvisValide;
+    // }
 
     public function isAvisRejete(): bool
     {
         return ($this->getStatut() == Avis::AVIS_STATUT_REJETE);
     }
-
+    //     public function setisAvisRejete(bool $newisAvisRejete)
+// {$this->isAvisRejete = $newisAvisRejete;
+// }
     public function isAvisOpen(): bool
     {
         return ($this->isAvisEnCours());
     }
+    // public function setisAvisOpen(bool $newisAvisOpen){
+    //     $this->isAvisOpen = $newisAvisOpen;
+    // }
 
     public function isAvisClosed(): bool
     {
         return (!$this->isAvisOpen());
     }
+    // public function setisAvisClosed(bool $newAvisClosed){
+    //     $this->isAvisClosed = $newAvisClosed;
+    // }
+
 }
 
 ?>
---
+++ modele/AvisDAO.php
@@ -0,0 +1,164 @@
+<?php
+class AvisDAO
+{
+    private PDO $pdo;
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getById(bool $loadData, int $avis_id, string $statut = "", string $commentaire = "", int $note = 0, ?Utilisateur $soumis_par, ?Utilisateur $valide_refuse_par = null, ?Commande $commande = null): Avis
+    {
+
+        $avisData = new Avis();
+        $avisData->setId($avis_id);
+        if ($loadData) {
+            $sql = "SELECT Avis_Id, Commentaire, Note, Statut, Soumis_par, Valide_refuse_par, Commande FROM avis WHERE Avis_Id =?";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([$avis_id]);
+            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($resultat) {
+
+                if ($resultat["Commentaire"] == null) {
+                    $avisData->setCommentaire("");
+                } else {
+                    $avisData->setcommentaire($resultat["Commentaire"]);
+                }
+                $avisData->setNote($resultat["Note"]);
+                $avisData->setStatut($resultat["Statut"]);
+
+                $utilisateur_id = $resultat["Soumis_par"];
+                $userDAO = new UtilisateurDAO($this->pdo);
+                $avisData->setSoumisPar($userDAO->getById(true, $utilisateur_id));
+
+                $utilisateur_id = $resultat["Valide_refuse_par"];
+                if ($utilisateur_id != null) {
+                    $userDAO = new UtilisateurDAO($this->pdo);
+                    $avisData->setValideRefusePar($userDAO->getById(true, $utilisateur_id));
+                }
+
+                $commande_id = $resultat["Commande"];
+                $avisData->setCommande(new Commande($commande_id, $this->pdo));
+            }
+        } else {
+            $avisData->setStatut($statut);
+            $avisData->setCommentaire($commentaire);
+            $avisData->setNote($note);
+            if ($soumis_par == null) {
+                $userDAO = new UtilisateurDAO($this->pdo);
+                $avisData->setSoumisPar($userDAO->getById(true, 0));
+            } else {
+                $avisData->setSoumisPar($soumis_par);
+            }
+            if ($valide_refuse_par == null) {
+                $userDAO = new UtilisateurDAO($this->pdo);
+                $avisData->setValideRefusePar($userDAO->getById(true, 0));
+            } else {
+                $avisData->setValideRefusePar($valide_refuse_par);
+            }
+            if ($commande == null) {
+                $avisData->setCommande(new Commande(0, $this->pdo));
+            } else {
+                $avisData->setCommande($commande);
+            }
+        }
+        return $avisData;
+    }
+
+
+
+    public function loadAvisIdOfCommande(int $commandeId): int
+    {
+        $sql = "SELECT Avis_Id FROM avis WHERE Commande = ?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$commandeId]);
+        $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($resultat) {
+            return $resultat['Avis_Id'];
+        }
+        return 0;
+    }
+
+    public function loadBestAvis(): array
+    {
+        // SQL : SELECT Avis_Id, Note, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = 'Validé' ORDER BY Note DESC LIMIT 3
+        $sql = "SELECT Avis_Id, Note, avis.Statut avisStatut, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo, Valide_refuse_par, Commande FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = ? ORDER BY Note DESC LIMIT 3";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([Avis::AVIS_STATUT_VALIDE]);
+        $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // print_r($resultat);
+        $avis = [];
+        if ($resultat) {
+            foreach ($resultat as $value) {
+                $userDAO = new UtilisateurDAO($this->pdo);
+                $new_avis = $this->getById(false, $value["Avis_Id"], $value["avisStatut"], $value["Commentaire"], $value["Note"], $userDAO->getById(true, $value["Soumis_par"]), $userDAO->getById(true, $value["Valide_refuse_par"]), new Commande($value["Commande"], $this->pdo));
+                array_push($avis, $new_avis);
+            }
+        }
+        return $avis;
+    }
+
+    public function loadAvisAValider(): array
+    {
+        // SQL : SELECT Avis_Id, Note, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = 'Validé' ORDER BY Note DESC LIMIT 3
+        $sql = "SELECT Avis_Id, Note, avis.Statut avisStatut, Commentaire, Soumis_par, utilisateur.Nom, utilisateur.Prenom, utilisateur.Pseudo, utilisateur.Photo, Valide_refuse_par, Commande FROM avis JOIN utilisateur ON avis.Soumis_par = utilisateur.Utilisateur_Id WHERE avis.Statut = ? ORDER BY Avis_Id DESC";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([Avis::AVIS_STATUT_EN_COURS]);
+        $resultat = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // print_r($resultat);
+        $avis = [];
+        if ($resultat) {
+            foreach ($resultat as $value) {
+                $userDAO = new UtilisateurDAO($this->pdo);
+
+                $new_avis = $this->getById(false, $value["Avis_Id"], $value["avisStatut"], $value["Commentaire"], $value["Note"], $userDAO->getById(true, $value["Soumis_par"]), $userDAO->getById(true), new Commande($value["Commande"], $this->pdo));
+                array_push($avis, $new_avis);
+            }
+        }
+        return $avis;
+    }
+
+    public function soumettreAvis(int $commandeId, int $note, string $commentaire, int $utilisateurId): Resultat
+    {
+        try {
+            $sql = "INSERT INTO avis (Commentaire, Note, Statut, Commande, Soumis_par) VALUES (?, ?, ?, ?, ?)";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([$commentaire, $note, Avis::AVIS_STATUT_EN_COURS, $commandeId, $utilisateurId]);
+            return new Resultat(true, Avis::RESULT_AVIS_SAVED);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return new Resultat(false, Avis::RESULT_FAIL);
+    }
+
+    public function validerAvis(int $avisId, int $employeId): Resultat
+    {
+        try {
+            $sql = "UPDATE avis SET Statut = ?, Valide_refuse_par = ? WHERE Avis_Id = ?";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([Avis::AVIS_STATUT_VALIDE, $employeId, $avisId]);
+            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+            return new Resultat(true, Avis::RESULT_AVIS_VALIDATED);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return new Resultat(false, Avis::RESULT_FAIL);
+    }
+
+    public function rejeterAvis(int $avisId, int $employeId): Resultat
+    {
+        try {
+            $sql = "UPDATE avis SET Statut = ?, Valide_refuse_par = ? WHERE Avis_Id = ?";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([Avis::AVIS_STATUT_REJETE, $employeId, $avisId]);
+            $resultat = $stmt->fetch(PDO::FETCH_ASSOC);
+            return new Resultat(true, Avis::RESULT_AVIS_REJECTED);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return new Resultat(false, Avis::RESULT_FAIL);
+    }
+
+
+} ?>
